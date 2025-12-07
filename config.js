ffc2b6e57d3750f59022544acc8b3fc6ac7f2ee8
enkhochirdatadeus <enkhochir.datadeus@outlook.com>
2025-12-07 21:29:21 +0800

    hci

---
+++ config.js
@@ -0,0 +1,7 @@
+// ICPC Math Contest Configuration
+// Keep this file private - do not share!
+
+const CONFIG = {
+  GEMINI_API_KEY: 'AIzaSyDuRQRanylIysgYZqH8XUOdKpt0RGGvcdw',
+  JUDGE_SECRET: 'judge2025'
+};
